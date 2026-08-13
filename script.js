1be7302798bcde3efd7bac41668c238135f2c834
tonytomo <fathonisatrioutomo@gmail.com>
2026-08-13 17:24:26 +0700

    feat: implement dynamic input sizing and initial math structure layout

---
+++ script.js
@@ -0,0 +1,13 @@
+const inputs = document.querySelectorAll('.auto-resize')
+
+inputs.forEach(input => {
+    input.addEventListener('input', (e) => {
+        const value = e.target.value;
+        const width = Math.max(value.length + 1, 2);
+        input.style.width = width + 'ch';
+    });
+
+    input.addEventListener('keydown', (e) => {
+        console.log(e.key)
+    });
+});
